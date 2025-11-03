a86d6eebe36df60e30bc4ceda1fe2efa9ea03b25
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-03 18:25:38 +0200

    Main menu window create

--- src/casino/MainMenu.java
+++ src/casino/MainMenu.java
@@ -1,7 +1,26 @@
 package casino;
 
-public class MainMenu {
+import java.awt.BorderLayout;
+import java.awt.Color;
 
-	//start
+import javax.swing.JFrame;
+
+public class MainMenu {
+	private final JFrame frame;
+
+    public MainMenu() {
+        frame = new JFrame("Slot Casino");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setResizable(false);
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(new Color(20, 30, 60));
+
+        createTitle();
+        createButtons();
+        createInfoButtons();
+
+        frame.setVisible(true);
+    }
 	
 }
