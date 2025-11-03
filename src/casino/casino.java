1097e5d77dd5ad8ea888deb3ce58c3bd764ccab6
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-03 16:11:08 +0200

    First class is changed

--- src/casino/casino.java
+++ src/casino/casino.java
@@ -1,4 +1,7 @@
 package casino;
+
+import javax.swing.SwingUtilities;
+
 /*
 casino
 ├── Casino.java        → Main class (start)
@@ -6,6 +9,9 @@ casino
 ├── GameLogic.java         → GAME LOGIC	(spin, animation)
 └── BalanceManager.java    → Balance, wins, formating
 */
+
 public class casino {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(casinoUI::new);
+    }
 }
