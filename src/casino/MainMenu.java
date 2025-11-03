2ab4b6e457873ee9361be191b0634bfd9f7ddf2c
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-03 18:26:15 +0200

    Create tittle

--- src/casino/MainMenu.java
+++ src/casino/MainMenu.java
@@ -2,8 +2,13 @@ package casino;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class MainMenu {
 	private final JFrame frame;
@@ -22,5 +27,16 @@ public class MainMenu {
 
         frame.setVisible(true);
     }
-	
+    
+    private void createTitle() {
+        JLabel title = new JLabel("CASINO SLOTS", SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 48));
+        title.setForeground(new Color(255, 215, 0));
+        title.setBorder(BorderFactory.createEmptyBorder(30, 0, 20, 0));
+
+        JPanel titlePanel = new JPanel(new BorderLayout());
+        titlePanel.setBackground(new Color(20, 30, 60));
+        titlePanel.add(title, BorderLayout.CENTER);
+        frame.add(titlePanel, BorderLayout.NORTH);
+    }
 }
