145a562a55ba1acad9483e561771a733730626ec
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-03 16:05:48 +0200

    Make maintance

--- src/casino/casino.java
+++ src/casino/casino.java
@@ -1,7 +1,11 @@
 package casino;
-
+/*
+casino
+├── SlotCasino.java        → Main class (start)
+├── CasinoUI.java          → Visual (GUI)
+├── GameLogic.java         → GAME LOGIC	(spin, animation)
+└── BalanceManager.java    → Balance, wins, formating
+*/
 public class casino {
-
-	//started
-	
+    
 }
