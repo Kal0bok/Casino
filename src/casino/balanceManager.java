ef610b117483e85ab1ed66f4c1fe7c34dbf5dbb9
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-03 16:48:15 +0200

    Making balance manager method

--- src/casino/balanceManager.java
+++ src/casino/balanceManager.java
@@ -4,5 +4,7 @@ public class balanceManager {
 	private long balance = 100_000_000_000_000L; // 100T
     private final casinoUI ui;
 
-	
+    public balanceManager(casinoUI ui) {
+        this.ui = ui;
+    }
 }
