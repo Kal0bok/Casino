010b17f14e0afb36455edd2c509407810b14d2fb
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-04 22:01:46 +0200

    Create new lobby

--- src/casino/casino.java
+++ src/casino/casino.java
@@ -9,7 +9,15 @@ casino
 ├── MainMenu.java           → Start window
 ├── AccountManager.java     ← Registration, login, save
 ├── GameLogic.java         → GAME LOGIC	(spin, animation)
-└── BalanceManager.java    → Balance, wins, formating
+├──BalanceManager.java    → Balance, wins, formating
+├── LobbyUI.java            ← New lobby screen (after login)
+├── AccountUI.java          ← Window profile player
+├── PokerUI.java            ← Poker game
+├── RouletteUI.java         ← Roulette game
+├── SpinsUI.java            ← Spins window (our old slots)
+├── CyberSportUI.java       ← CyberSport game
+├── VirtualSportUI.java     ← ЗаглVirtualSport game
+├── SportUI.java            ← Sport game
 */
 
 public class casino {
