9fa8a2cad81efbd752fd8796e992f64ef522641a
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-04 22:05:04 +0200

    Make lobby window

--- src/casino/LobbyUI.java
+++ src/casino/LobbyUI.java
@@ -1,7 +1,33 @@
 package casino;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class LobbyUI {
 
-	//started
+	private JFrame frame;
+    private final AccountManager.Player player;
+    private final AccountManager accountManager;
+
+    public LobbyUI(AccountManager.Player player, AccountManager accountManager) {
+        this.player = player;
+        this.accountManager = accountManager;
+        initUI();
+    }
+
+    private void initUI() {
+        frame = new JFrame("Casino Lobby");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 700);
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(new Color(15, 30, 60));
+        frame.setLocationRelativeTo(null);
+
+        createTopBar();
+        createMainContent();
+
+        frame.setVisible(true);
+    }
 	
 }
