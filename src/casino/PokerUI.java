b0b0e5888bff463540769dd0da0912cbecb671c7
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-04 22:16:52 +0200

    Finished class poker

--- src/casino/PokerUI.java
+++ src/casino/PokerUI.java
@@ -1,7 +1,16 @@
 package casino;
 
-public class PokerUI {
-	
-	//start
+import javax.swing.*;
+import java.awt.*;
 
+public class PokerUI {
+    public PokerUI(AccountManager.Player player, AccountManager accountManager) {
+        JFrame frame = new JFrame("Poker");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(new Color(20, 30, 60));
+        frame.add(new JLabel("Poker game will be here soon 🎴", SwingConstants.CENTER));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
